00a8719521be5e04b3b028d5a37c496419717205
aabusha3 <90729952+aabusha3@users.noreply.github.com>
2022-09-26 11:39:32 -0400

    Add task3 file

---
+++ task3/251149713_lab1_module 2_task3.py
@@ -0,0 +1,97 @@
+from socket import *
+
+msg = "\r\n I love computer networks!" 
+endmsg = "\r\n.\r\n"
+
+
+# Choose a mail server (e.g. Google mail server) and call it mailserver 
+mailserver = 'smtp.gmail.com'
+
+
+# Create socket called clientSocket and establish a TCP connection with mailserver
+
+#Fill in start
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((mailserver,25))
+
+#Fill in end
+
+recv = clientSocket.recv(1024).decode() 
+print(recv)
+if recv[:3] != '220':
+    print('220 reply not received from server.')
+
+
+
+
+# Send HELO command and print server response. 
+heloCommand = 'HELO Alice\r\n' 
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode() 
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+
+
+
+# Send MAIL FROM command and print server response.
+ 
+mailFromCommand = 'MAIL FROM: <mail@gmail.com>\r\n' 
+clientSocket.send(mailFromCommand.encode())
+recv2 = clientSocket.recv(1024).decode() 
+print(recv2)
+if recv2[:3] != '250':
+    print('250 reply not received from server.')
+
+
+# Send RCPT TO command and print server response.
+
+# Fill in start
+
+
+
+
+# Fill in end
+
+
+
+
+# Send DATA command and print server response.
+
+# Fill in start
+
+
+
+
+# Fill in end
+
+
+
+
+# Send message data.(auth)354
+
+# Fill in start
+
+
+
+
+# Fill in end
+
+# Message ends with a single period.
+
+# Fill in start
+
+# Fill in end
+
+
+
+
+# Send QUIT command and get server response.221
+
+# Fill in start
+
+
+
+
+# Fill in end
