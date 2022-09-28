9943a32f2c0dd92edf53bab469f8c3dca5be88d5
aabusha3 <aabusha3@uwo.ca>
2022-09-28 08:05:56 -0400

    Attempt at auth and tls secure socket

--- task3/251149713_lab1_module 2_task3.py
+++ task3/251149713_lab1_module 2_task3.py
@@ -1,18 +1,12 @@
 from socket import *
-
+import ssl, base64
 subject = "STMP mail sent from py"
 msg = "\r\n I love computer networks!" 
 endmsg = "\r\n.\r\n"
 
 
 # Choose a mail server (e.g. Google mail server) and call it mailserver 
-mailserver = 'smtp.gmail.com'
-
-# enter user's gmail adress below
-user = 'ahmadzaidsami@gmail.com'
-
-# enter destinations's gmail adress below
-receiver = 'ahmadzaidsami@gmail.com'
+mailserver = ('smtp.gmail.com', 587)
 
 
 # Create socket called clientSocket and establish a TCP connection with mailserver
@@ -22,34 +16,15 @@ receiver = 'ahmadzaidsami@gmail.com'
 # 465 is gmails ssl
 # 587 is gmails TLS
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((mailserver,587))
+
+clientSocket.connect(mailserver)
 
 #Fill in end
 
-
-#############
-# udp example
-
-# get user keyboard input
-# message = raw_input(’Input lowercase sentence:’)
-
-# attach server name, port to message; send into socket
-# clientSocket.sendto(message.encode(), (serverName, serverPort))
-
-# read reply characters from socket into string
-# modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-
-# print out received string and close socket
-# print modifiedMessage.decode()
-# clientSocket.close()
-#############
-
-
 recv = clientSocket.recv(1024).decode() 
-print(recv)
+print('1: '+ recv)
 if recv[:3] != '220':
-    print('220 reply not received from server.')
-
+    print('220 reply not received from server.1')
 
 
 
@@ -57,21 +32,58 @@ if recv[:3] != '220':
 heloCommand = 'HELO Alice\r\n' 
 clientSocket.send(heloCommand.encode())
 recv1 = clientSocket.recv(1024).decode() 
-print(recv1)
+print('2: '+ recv1)
 if recv1[:3] != '250':
-    print('250 reply not received from server.')
+    print('250 reply not received from server.2')
 
+tlsCommand = 'STARTTLS\r\n'
+clientSocket.send(tlsCommand.encode())
+recva = clientSocket.recv(1024).decode()
+print("a: " + recva)
+if recva[:3] != '220':
+   print('220 reply not received from server.a')
+
+#clientSocket = ssl.wrap_socket(clientSocket, ssl_version=ssl.PROTOCOL_SSLv23) 
+clientSocket = ssl.wrap_socket(clientSocket, ssl_version=ssl.PROTOCOL_TLSv1)
+
+#auth login
+authMsg = "AUTH PLAIN\r\n"
+clientSocket.send(authMsg.encode())
+recvb = clientSocket.recv(1024).decode()
+print("b: " + recvb)
+if recvb[:3] != '250':
+    print('250 reply not received from server.b')
+
+
+username = "ahmadzaidsami"
+password = "AhmadZaidSami2014"
+auth = username + "\0" + password
+base64_str = ('%s\0%s' % (username, password)).encode()
+auth = base64.b64encode(base64_str)
+clientSocket.send(auth)
+recvy = clientSocket.recv(1024).decode()
+print("y: " + recvy)
+if recvy[:3] != '250':
+    print('250 reply not received from server.y')
+
+
+
+# enter sender's gmail adress below
+sender = 'ahmadzaidsami@gmail.com'
 
 
 # Send MAIL FROM command and print server response.
  
-mailFromCommand = 'MAIL FROM: <'+ user +'>\r\n' 
+mailFromCommand = 'MAIL FROM: <'+ sender +'>\r\n' 
 clientSocket.send(mailFromCommand.encode())
 recv2 = clientSocket.recv(1024).decode() 
-print(recv2)
+print('3: '+ recv2)
 if recv2[:3] != '250':
-    print('250 reply not received from server.')
+    print('250 reply not received from server.3')
 
+
+# enter receiver's gmail adress below
+receiver = 'ahmadzaidsami@gmail.com'
 
 # Send RCPT TO command and print server response.
 
@@ -79,12 +91,9 @@ if recv2[:3] != '250':
 rcptToCommand = 'RCPT TO: <'+ receiver +'>\r\n'
 clientSocket.send(rcptToCommand.encode())
 recv3 = clientSocket.recv(1024).decode() 
-print(recv3)
+print('4: '+ recv3)
 if recv3[:3] != '250':
-    print('250 reply not received from server.')
-
-
-
+    print('250 reply not received from server.4')
 # Fill in end
 
 
@@ -96,16 +105,16 @@ if recv3[:3] != '250':
 dataCommand = 'DATA\r\n'
 clientSocket.send(dataCommand.encode())
 recv4 = clientSocket.recv(1024).decode() 
-print(recv4)
+print('5: '+ recv4)
 if recv4[:3] != '250':
-    print('250 reply not received from server.')
+    print('250 reply not received from server.5')
 
 msgCommand = "Subject: " + subject + "\r\n\r\n" + msg +"\r\n"+ endmsg +"\r\n"
 clientSocket.send(msgCommand.encode())
 recv5 = clientSocket.recv(1024).decode() 
-print(recv5)
+print('6: '+ recv5)
 if recv5[:3] != '354':
-    print('354 reply not received from server.')
+    print('354 reply not received from server.6')
 
 # Fill in end
 
@@ -136,9 +145,9 @@ if recv5[:3] != '354':
 quitCommand = 'QUIT\r\n'
 clientSocket.send(quitCommand.encode())
 recv6 = clientSocket.recv(1024).decode() 
-print(recv6)
+print('7: '+ recv6)
 if recv6[:3] != '221':
-    print('221 reply not received from server.')
+    print('221 reply not received from server.7')
 clientSocket.close()
 
 
